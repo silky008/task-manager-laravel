a88560a3f4dd515cf06ef7adf60c69a0b1668486
Silky <silky@localhost.com>
2026-07-03 12:49:30 +0200

    Added delete task API with error handling

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -51,4 +51,19 @@ class TaskController extends Controller
             'task'    => $task,
         ]);
     }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        if (! $task) {
+            return response()->json([
+                'message' => 'Task not found',
+            ], 404);
+        }
+        $task->delete();
+        return response()->json([
+            'message' => 'Task deleted successfully',
+        ]);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,5 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 
 Route::patch('/tasks/{id}', [TaskController::class, 'update']);
+
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
